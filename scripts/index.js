172c607ad44def82e89654c9998f63658346cd94
Mathieu Leplatre <mathieu@mozilla.com>
2015-08-15 16:46:46 +0200

    Generate uuid for local user

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,26 @@
 import "babel/polyfill";
 import btoa from "btoa";
+import { v4 as uuid4 } from "uuid";
 import React from "react";
 import App from "./components/App";
 import { Store } from "./store";
 import Kinto from "kinto";
 
-// Take username from location hash (With default value):
-const user = window.location.hash = (window.location.hash.slice(1) || "public-demo");
-const userpass64 = btoa(user + ":s3cr3t");
+// Take username from location hash or local storage.
+var user = window.location.hash.slice(1);
+if (!user) {
+  user = window.localStorage.getItem("lastuser") || uuid4();
+  window.localStorage.setItem("lastuser", user);
+  window.location.hash = user;
+}
 
 // Use Mozilla demo server with Basic authentication:
 const server = "https://kinto.dev.mozaws.net/v1";
+const userpass64 = btoa(user + ":s3cr3t");
 const auth = "Basic " + userpass64;
 const kinto = new Kinto({remote: server, headers: {Authorization: auth}});
 
-const store = new Store(kinto, "items");
+const store = new Store(kinto, "routina-v1");
 
 // Make sure local data depend on current user.
 // Note: Kinto.js will have an option: https://github.com/Kinto/kinto.js/pull/111
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: [
     "webpack-dev-server/client?http://localhost:3000",
     "webpack/hot/only-dev-server",
-    "./scripts/index"
+    "./scripts/index",
     "./scripts/models"
   ],
   output: {
