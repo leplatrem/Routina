087c35e9195c677cb66fe80bf796c5ad174385af
Mathieu Leplatre <mathieu@mozilla.com>
2015-08-18 18:01:24 +0200

    Minor improvement in margins and borders

--- scripts/components/App.js
+++ scripts/components/App.js
@@ -117,7 +117,7 @@ export class Item extends React.Component {
           </span>
           <span className="last">{last}</span>
         </div>
-        <button onClick={this.props.onEdit} className="btn edit" aria-label="Edit">
+        <button onClick={this.props.onEdit} className="btn default edit" aria-label="Edit">
           <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
         </button>
       </li>
@@ -224,7 +224,10 @@ export default class App extends React.Component {
           <Form saveRecord={this.createRecord.bind(this)}/>
         </div>
         <div className="hbox">
-          <button className="btn sync fit" data-icon="sync" onClick={this.syncRecords.bind(this)} disabled={disabled}> Sync</button>
+          <button className="btn default sync fit" data-icon="sync" onClick={this.syncRecords.bind(this)} disabled={disabled}>
+          <span className="glyphicon glyphicon-cloud-upload" aria-hidden="true"></span>
+          &nbsp;Sync
+          </button>
         </div>
       </section>
     );
