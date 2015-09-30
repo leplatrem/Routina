3840d3baba2e6a0d18e871a9ce12663cc8465184
Mathieu Leplatre <mathieu@mozilla.com>
2015-09-30 15:56:01 +0200

    Upgrade Kinto.js

--- scripts/index.js
+++ scripts/index.js
@@ -21,15 +21,11 @@ auth.authenticate(window.location.hash.slice(1));
 window.location.hash = auth.token;
 
 const headers = Object.assign({}, auth.headers);
-const kinto = new Kinto({remote: server, headers: headers});
+const kinto = new Kinto({remote: server, dbPrefix: auth.token, headers: headers});
 
 const store = new Store(kinto, "routina-v1");
 store.online = window.navigator.onLine;
 window.addEventListener("offline", () => {store.online = false;});
 window.addEventListener("online", () => {store.online = true;});
-
-// Make sure local data depend on current user.
-// Note: Kinto.js will have an option: https://github.com/Kinto/kinto.js/pull/111
-store.collection.db.dbname = auth.token + store.collection.db.dbname;
 
 React.render(<App store={store} auth={auth}/>, document.getElementById("app"));
